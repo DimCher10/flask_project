6404251ba78b8fc6b72cb65db0b46fa1e6a524df
DimCher10 <103523226+DimCher10@users.noreply.github.com>
2022-04-27 21:12:16 +0300

    images added

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# flask_project
-This is my first flask project.
+# Flask Projects for beginners
+
+## Project 1
+
+### Part 1
+
+The task is to place a die throw result on the main page.  
+_Example_  
+```You have thrown the dice, result is 6```
+### Part 2
+Add an image to the first project
+
+<img width=200 src="https://www.pikpng.com/pngl/m/326-3267933_dice-clipart-number-3-clip-art-dice-face.png">
--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
+from random import randint
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
 def index():
    return render_template("index.html")
+
+@app.route("/about")
+def  about():
+    return render_template("about.html")
+
+@app.route("/random")
+def random():
+    number  =  randint(1, 6)
+    with open("random.txt", "w") as f:
+        f.write(f"... {number}")
+    return render_template("random.html", result=number)
+    # Вы бросили кубик, выпало число 5
 
 app.run()
